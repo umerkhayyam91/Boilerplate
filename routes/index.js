881701e4df85b499a661ab18efebec0e42b4c1eb
umerkhayyam91 <umerkhayyam91@gmail.com>
2023-11-14 18:12:35 +0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const authController = require('../controller/authController');
+const auth = require('../middlewares/auth');
+
+const router = express.Router();
+
+// user
+
+// register
+router.post('/register', authController.register);
+
+// login
+router.post('/login', authController.login);
+
+// logout
+router.post('/logout', auth, authController.logout)
+
+// refresh
+router.get('/refresh', authController.refresh);
+
+module.exports = router;
